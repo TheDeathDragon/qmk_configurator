eb392c35d313243c1839cc8550cca61691063c73
TheDeathDragon <me@shiro.la>
2025-01-22 01:55:23 +0800

    Add tdd75

--- src/store/modules/constants.js
+++ src/store/modules/constants.js
@@ -10,11 +10,9 @@ if (isUndefined(keyboards_baseurl)) {
   throw 'Keyboard Metadata URL has not been specified';
 }
 export const backend_keyboards_url = `${keyboards_baseurl}/v1/keyboards`;
-export const backend_keyboard_list_url = `${keyboards_baseurl}/v1/keyboard_list.json`;
+export const backend_keyboard_list_url = `${keyboards_baseurl}/keyboard_list.json`;
 export const backend_compile_url = `${backend_baseurl}/v1/compile`;
 export const backend_status_url = `${backend_baseurl}/v1`;
-export const backend_readme_url_template = template(
-  `${backend_keyboards_url}/<%= keyboard %>/readme.md`
-);
+export const backend_readme_url_template = template(`readme.md`);
 export const backend_skeletons_url = `${backend_baseurl}/v1/skeletons`;
 export const PREVIEW_LABEL = 'Preview info.json';
